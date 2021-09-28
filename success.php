c2c0d89bc2413eecd6328f61c96e6d8cc535784a
huynguyen52 <61266768+huynguyen52@users.noreply.github.com>
2021-09-28 18:21:55 +0700

    Update success.php

--- success.php
+++ success.php
@@ -8,5 +8,21 @@
 <body>
 	<h1>Đăng nhập thành công</h1>
 	<?php session_start(); echo($_SESSION["test"])?>
+	<form action="success.php" id="form" method="GET">
+		<div class="form-control">
+			<label for="">What's your name?</label>
+			<input type="text" name="name" placeholder="Type your name..">
+		</div>
+		<button type="submit">Ok</button>
+	</form>
+	<?php 
+	if(isset($_GET["name"])){
+		$username = $_GET["name"];	
+		if($username){
+			echo "Hello $username";
+		}
+	}	
+	?>
+	
 </body>
 </html>
